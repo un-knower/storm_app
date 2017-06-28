6dc5a84f8380b0b062ae4e94082f3d33b7963ab6
wangchuanyi(王传义.技术中心.IM开发部) <wangchuanyi@ds.gome.com.cn>
2017-06-28 17:50:54 +0800

    add conf

---
+++ Flume_ng/Flume-Interceptor/src/main/java/com/flume/interceptor/AppendIPBuilder.java
@@ -0,0 +1,18 @@
+package com.flume.interceptor;
+
+import org.apache.flume.Context;
+import org.apache.flume.interceptor.Interceptor;
+
+public class AppendIPBuilder implements Interceptor.Builder{
+
+	private String serviceId=null;
+	public void configure(Context context) {
+		String configServiceId=context.getString("serviceId");
+		serviceId=configServiceId;
+	}
+
+	public Interceptor build() {
+		return new AppendIPInterceptor(serviceId);
+	}
+
+}
